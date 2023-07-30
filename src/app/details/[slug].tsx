f023943558fd16b6b472c539c74246593f3b9e19
Bernardo Munz <bernardo.munz@ivegroup.com.au>
2023-07-31 07:38:53 +1000

    add type

--- src/app/details/[slug].tsx
+++ src/app/details/[slug].tsx
@@ -2,12 +2,13 @@ import { useLocalSearchParams } from "expo-router";
 import React, { useEffect } from "react";
 import { View, Text } from "react-native";
 import { openDatabase } from "../../service/sqlite";
+import { IPlant } from "../../models/plantsModel";
 
 const db = openDatabase();
 function DetailsScreen() {
   const { slug } = useLocalSearchParams();
   // const [isLoading, setIsLoading] = React.useState(false);
-  const [info, setInfo] = React.useState<any[]>();
+  const [info, setInfo] = React.useState<IPlant[]>();
 
   useEffect(() => {
     db.transaction(
